0a4b810dc0442ef23026b735848308e39a03d931
Haider Cheema <haidrrcheema40@gmail.com>
2021-10-15 16:08:48 -0700

    title changed in show file

--- show.php
+++ show.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap-4.6/css/bootstrap.min.css">
-    <title>Sunday Crud</title>
+    <title>Sunday Crud With php</title>
 </head>
 
 <body>
